fdb3661c5e220d6fc5211475f3e9d90da2f577d8
Yash Jatin Dave <yash.dave@MacBook-Pro.local>
2023-01-28 15:39:10 +0530

    initial_commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yashdave00/bittorrent-client-go
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jackpal/bencode-go v1.0.0
+)
---
+++ torrentfile/torrentfile.go
@@ -0,0 +1,42 @@
+package torrentfile
+
+import (
+	"io"
+
+	"github.com/jackpal/bencode-go"
+)
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+func (bto bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+
+}
+
+// Open parses a torrent file
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(r, &bto)
+	if err != nil {
+		return nil, err
+	}
+	return &bto, nil
+}
